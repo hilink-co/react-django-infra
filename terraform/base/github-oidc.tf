50d75a95bd520541b8dfcff93e4b429eed7e781e
Marshall Shen <shen.marshall@gmail.com>
2023-10-21 16:24:09 -0500

    update github-oidc (#3)

--- terraform/base/github-oidc.tf
+++ terraform/base/github-oidc.tf
@@ -5,7 +5,10 @@ module "github-oidc" {
   create_oidc_provider = true
   create_oidc_role     = true
 
-  repositories              = ["marshallshen/react-django-apps"]
+  repositories              = [
+    "marshallshen/web-apps-infra",
+    "marshallshen/react-django-apps"
+  ]
   oidc_role_attach_policies = [
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
     "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
